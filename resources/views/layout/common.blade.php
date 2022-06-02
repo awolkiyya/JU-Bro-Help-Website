c8e50cca095889a6b267def4e50cc15ec672d278
awol <awolabdulbaasit143@gmail.com>
2022-06-02 00:37:47 -0700

    sending this changes to repository

--- resources/views/layout/common.blade.php
+++ resources/views/layout/common.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+  {{-- some changes are there then  --}}
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
